98121135d94019911c5355d7b77ebb51345bc66a
Sean Corfield <sean@corfield.org>
2015-01-09 18:25:29 -0800

    Since we had map + filter, I refactored to a reduce instead

--- src/ordered_subset/core.clj
+++ src/ordered_subset/core.clj
@@ -4,5 +4,9 @@
   "Given a nested map and a sequence of desired keys in order from it,
   return a sequence of the selected nested pieces, with label added to each."
   [set subset]
-  (map (fn [label] (assoc (get set label) :label (name label)))
-       (filter (fn [label] (get set label)) subset)))
+  (reduce (fn [ordered label]
+            (if-let [item (get set label)]
+              (conj ordered (assoc item :label (name label)))
+              ordered))
+          []
+          subset))
