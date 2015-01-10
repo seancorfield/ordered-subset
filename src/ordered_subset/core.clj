630ff0b8e8b21ff17b726d7abef28243376de227
Sean Corfield <sean@corfield.org>
2015-01-09 18:21:29 -0800

    Update code to satisfy new tests (unknown missing are omitted from ordered subset)

--- src/ordered_subset/core.clj
+++ src/ordered_subset/core.clj
@@ -4,4 +4,5 @@
   "Given a nested map and a sequence of desired keys in order from it,
   return a sequence of the selected nested pieces, with label added to each."
   [set subset]
-  (map (fn [label] (assoc (get set label) :label (name label))) subset))
+  (map (fn [label] (assoc (get set label) :label (name label)))
+       (filter (fn [label] (get set label)) subset)))
